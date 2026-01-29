068523abb34df998d0703913228fb177f678510c
Qwertz Studio <obrienandrew2012@gmail.com>
2026-01-29 16:17:27 -0600

    Implement player health and combat system
    Added player health and creature encounter mechanics.

--- dungeons.py
+++ dungeons.py
@@ -1,38 +1,59 @@
 import random
+
 print("HELLO DUNGEON CRAWLER!")
+
+player_hp = 30  # your health
 
 answer = input("ARE YOU READY TO GO IN THE DUNGEON??? Y/N ")
 
 if answer == "N":
     input("ok bye, hit enter to leave")
-# the code :)
+    exit()
 
 elif answer == "Y":
     quest1 = input("GREAT! you're there now GOOD LUCK!! ")
     print(quest1)
-smallcreatures = print("Small Creatures")
+
 print("*hear that?*")
 print("*eyes glow in the dark*")
-beast1 = input("SMALL CREATURES APPEAR! FIGHT, CHECK THEIR STATS, OR RUN? F/C/S ")
-beast2 = print(f"{smallcreatures} deal 5 Damage to you!")
-    print(f"You have {health} HP left!")
-health = (int(100))
+
+# --- Creature list ---
+creatures = ["Goblin", "Slime", "Skeleton", "Bat", "Tiny Demon"]
+creature = random.choice(creatures)
+
+print(f"A {creature} appears!")
+
+beast1 = input("FIGHT, CHECK THEIR STATS, OR RUN? F/C/S ")
+
+if beast1 == "C":
+    print(f"{creature} HP: 10")
+    print("Damage per hit: 5")
+    beast1 = input("Now what? FIGHT OR RUN? F/S ")
+
+if beast1 == "S":
+    print("You run away safely...")
+    input("Press ENTER to exit")
+    exit()
+
 if beast1 == "F":
-    number = random.randint(1, 5)
+    # --- Player attack ---
+    player_damage = random.randint(8, 20)
+    print(f"You attack the {creature}!")
+    print(f"You deal {player_damage} damage!")
 
-    if number == 5:
-        print("CRITICAL HIT!")
-        print("20 DAMAGE")
+    # --- Creature attack ---
+    creature_damage = random.randint(3, 8)
+    player_hp -= creature_damage
+
+    print(f"The {creature} hits you back for {creature_damage} damage!")
+    print(f"Your HP is now {player_hp}")
+
+    if player_hp <= 0:
+        print("You died in the dungeon...")
     else:
-        print("You attack!")
-        print("12 DAMAGE")
-        print("Creatures have 8 HP left!")
-        print(beast1)
-elif beast1 == "C":
-    print("Small Creatures: 20 HP, 5 Damage Per Hit 'Simple Enemies'")
-    print(beast1)
-else: print("You Ran! Earned Nothing")
+        print("You survived the encounter!")
 
-input("press enter to exit dungeon")
+input("\nPress ENTER to exit game")
+
     
   # ✈︎🕈︎☜︎☼︎❄︎☪︎ 💧︎❄︎🕆︎👎︎✋︎⚐︎ ☝︎✌︎💣︎☜︎💧︎
