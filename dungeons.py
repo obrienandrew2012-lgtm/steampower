b90d53da4cef4aa04a85b3b237dcff5cc10499b1
Qwertz Studio <obrienandrew2012@gmail.com>
2026-01-29 15:54:28 -0600

    Update dungeons.py

--- dungeons.py
+++ dungeons.py
@@ -1,12 +1,29 @@
+import random
 print("HELLO DUNGEON CRAWLER!")
 
 answer = input("ARE YOU READY TO GO IN THE DUNGEON??? Y/N ")
 
 if answer == "N":
     input("ok bye, hit enter to leave")
+# the code :)
 
 elif answer == "Y":
     quest1 = input("GREAT! you're there now GOOD LUCK!! ")
     print(quest1)
 
+print("*hear that?*")
+print("*eyes glow in the dark*")
+beast1 = input("SMALL CREATURES APPEAR! FIGHT, CHECK THEIR STATS, OR RUN? F/C/S ")
+
+if beast1 == "F":
+    number = random.randint(1, 5)
+
+    if number == 5:
+        print("CRITICAL HIT!")
+        print("20 DAMAGE")
+    else:
+        print("You attack!")
+        print("12 DAMAGE")
+elif 
+    
   # ✈︎🕈︎☜︎☼︎❄︎☪︎ 💧︎❄︎🕆︎👎︎✋︎⚐︎ ☝︎✌︎💣︎☜︎💧︎
