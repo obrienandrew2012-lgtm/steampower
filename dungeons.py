9e212252104550f9c72ef4721fa24cf5e1acf1ef
Qwertz Studio <obrienandrew2012@gmail.com>
2026-01-29 15:41:39 -0600

    DUNGEON CRAWLER.py

---
+++ dungeons.py
@@ -0,0 +1,12 @@
+print("HELLO DUNGEON CRAWLER!")
+
+answer = input("ARE YOU READY TO GO IN THE DUNGEON??? Y/N ")
+
+if answer == "N":
+    input("ok bye, hit enter to leave")
+
+elif answer == "Y":
+    quest1 = input("GREAT! you're there now GOOD LUCK!! ")
+    print(quest1)
+
+  # ✈︎🕈︎☜︎☼︎❄︎☪︎ 💧︎❄︎🕆︎👎︎✋︎⚐︎ ☝︎✌︎💣︎☜︎💧︎
